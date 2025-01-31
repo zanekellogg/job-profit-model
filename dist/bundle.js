988cb27c18f44a1edbe8453d21f2245d754a18da
Zane Kellogg <zane.kellogg@gmail.com>
2025-01-31 15:00:45 -0600

    Finishing variance charts

--- dist/bundle.js
+++ dist/bundle.js
@@ -224,6 +224,7 @@ var App = (() => {
   var profitChart;
   var varianceCtx = document.getElementById("varianceCtx");
   var varianceCard = document.getElementById("varianceCard");
+  var varianceChart;
   function buildCharts(data) {
     console.log("Build charts called");
     const biData = getGroupedMonthlyIntelligenceData(data);
@@ -239,6 +240,7 @@ var App = (() => {
       varianceCard.innerText = "-$" + avgVariance * -1;
       varianceCard.classList.add("text-danger");
     }
+    varianceChart = new Chart(varianceCtx, getVarianceChartConfiguration(biData));
   }
   function getProfitChartConfiguration(biData) {
     if (profitChart) {
@@ -269,26 +271,58 @@ var App = (() => {
             position: "left",
             title: { display: true, text: "Profit Percent" },
             beginAtZero: true,
-            ticks: PercentScaleTickConfig()
+            ticks: percentScaleTickConfig()
           },
           "y-right": {
             position: "right",
             title: { display: true, text: "Total Profit" },
             beginAtZero: true,
-            ticks: DollarScaleTickConfig()
+            ticks: dollarScaleTickConfig()
           }
         }
       }
     };
   }
-  function PercentScaleTickConfig() {
+  function getVarianceChartConfiguration(biData) {
+    if (varianceChart) {
+      varianceChart.destroy();
+    }
+    return {
+      type: "line",
+      data: {
+        labels: biData.map((e) => e.dateLabel),
+        datasets: [{
+          label: "Total Estimate vs Actual",
+          data: biData.map((e) => e.variance),
+          borderWidth: 1,
+          yAxisID: "y-left"
+        }]
+      },
+      options: {
+        scales: {
+          "y-left": {
+            position: "left",
+            title: { display: true, text: "Estimate vs Actual" },
+            beginAtZero: true,
+            ticks: dollarScaleTickConfig(),
+            grid: {
+              color: (context) => context.tick.value === 0 ? "black" : "rgba(0, 0, 0, 0.1)"
+              // Bold zero line
+              // lineWidth: (context) => context.tick.value === 0 ? 2 : 1 // Thicker zero line
+            }
+          }
+        }
+      }
+    };
+  }
+  function percentScaleTickConfig() {
     return {
       callback: function(value) {
         return value + "%";
       }
     };
   }
-  function DollarScaleTickConfig() {
+  function dollarScaleTickConfig() {
     return {
       callback: function(value) {
         return "$" + value.toLocaleString();
--- src/charts.js
+++ src/charts.js
@@ -32,6 +32,7 @@ export function buildCharts(data) {
         varianceCard.innerText = '-$' + (avgVariance * -1);
         varianceCard.classList.add('text-danger');
     }
+    varianceChart = new Chart(varianceCtx, getVarianceChartConfiguration(biData));
 }
 
 function getProfitChartConfiguration(biData)
@@ -61,20 +62,52 @@ function getProfitChartConfiguration(biData)
                     position: 'left',
                     title: { display: true, text: 'Profit Percent'},
                     beginAtZero: true,
-                    ticks: PercentScaleTickConfig()
+                    ticks: percentScaleTickConfig()
                 },
                 'y-right': {
                     position: 'right',
                     title: { display: true, text: 'Total Profit'},
                     beginAtZero: true,
-                    ticks: DollarScaleTickConfig()
+                    ticks: dollarScaleTickConfig()
                 }
             }
         }
     };
 }
 
-function PercentScaleTickConfig()
+function getVarianceChartConfiguration(biData)
+{
+    if (varianceChart) { varianceChart.destroy(); }
+
+    return {
+        type: 'line',
+        data: {
+            labels: biData.map(e => e.dateLabel),
+            datasets: [{
+                label: 'Total Estimate vs Actual',
+                data: biData.map(e => e.variance),
+                borderWidth: 1,
+                yAxisID: 'y-left'
+            }]
+        },
+        options: {
+            scales: {
+                'y-left': {
+                    position: 'left',
+                    title: { display: true, text: 'Estimate vs Actual'},
+                    beginAtZero: true,
+                    ticks: dollarScaleTickConfig(),
+                    grid: {
+                        color: (context) => context.tick.value === 0 ? "black" : "rgba(0, 0, 0, 0.1)", // Bold zero line
+                        // lineWidth: (context) => context.tick.value === 0 ? 2 : 1 // Thicker zero line
+                    },
+                }
+            }
+        }
+    };
+}
+
+function percentScaleTickConfig()
 {
     return {
         callback: function(value) {
@@ -83,7 +116,7 @@ function PercentScaleTickConfig()
     };
 }
 
-function DollarScaleTickConfig()
+function dollarScaleTickConfig()
 {
     return {
         callback: function(value) {
